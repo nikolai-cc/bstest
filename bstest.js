b124d53511767c43d3d80de32106b712650d4952
Nikolai Gillissen <nikolai@altijddesign.nl>
2018-02-10 21:46:34 +0100

    Typo in bstest.js

--- bstest.js
+++ bstest.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 if(window.CSS.supports("display", "grid")){
-    document.querySelectorAll(".bstest").forEach(function(d) {d.parentNode.removeChild()});
+    document.querySelectorAll(".bstest").forEach(function(d) {d.parentNode.removeChild(d)});
 } else {
     document.querySelector('#bstestlabel').innerHTML = 'This site was built for modern, safe browsers. Please <a href=\"http://whatbrowser.org/\"style=\"color:darkred;font-weight:bold;text-decoration:none;\">update your browser</a> for a correctly working website.<span style="position:absolute;right:0">(Click to dismiss)</span>';
 }
